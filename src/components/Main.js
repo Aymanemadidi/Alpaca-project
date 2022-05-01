4afaa3d44ed59c0e8d3c27595366bb2f54366fa1
Aymane Elmadidi <aymanelmadidi@gmail.com>
2022-05-01 02:09:08 +0000

    finished but still needs some styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -241,13 +241,13 @@ function Main() {
               className="bg-navbar text-white px-4 py-2 rounded-md"
               onClick={() => downloadPng()}
             >
-              download
+              Download
             </button>
             <button
               className="ml-4 bg-navbar text-white px-4 py-2 rounded-md"
               onClick={() => random()}
             >
-              random
+              Random
             </button>
           </div>
         </div>
